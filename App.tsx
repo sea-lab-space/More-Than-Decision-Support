a7966fb5c96eeae336951878166693db971018a7
Minadocyc <cycsukismh@gmail.com>
2026-02-26 23:23:43 +0800

    clean up

--- App.tsx
+++ App.tsx
@@ -1,18 +1,7 @@
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import HomePage from "./src/pages/HomePage";
-import DevPage from "./src/pages/DevPage";
 import PubPage from "@/pages/PubPage";
 
 export default function App() {
     return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<PubPage />} />
-          <Route path="/mind" element={<HomePage />} />
-          <Route path="/mind/:patientId" element={<HomePage />} />
-          <Route path="/test" element={<DevPage />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Router>
+      <PubPage />
     );
 }
